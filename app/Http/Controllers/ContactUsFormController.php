f891c0c48903acfadbae9108ecba35ae92a59a59
balwinder4264 <itsupport5@opulence.com>
2021-09-27 15:50:55 -0400

    added route to show the form

--- app/Http/Controllers/ContactUsFormController.php
+++ app/Http/Controllers/ContactUsFormController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ContactUs;
 
 class ContactUsFormController extends Controller
 {
     //
+    public function createForm(Request $request) {
+        return view('contact');
+      }
+  
+      // Store Contact Form data
+    
 }
